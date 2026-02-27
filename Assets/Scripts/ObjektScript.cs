2a2a348c78e4aa15088cdfa40326b93a72dbbf63
hjortshojmagnus-source <hjortshojmagnus@gmail.com>
2026-02-27 12:42:35 +0100

    basis objekter

---
+++ Assets/Scripts/ObjektScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class ObjektScript : MonoBehaviour
+{
+    // lives for this object, initialized based on its tag
+    public int lives;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // assign lives according to tag
+        if (CompareTag("Stone"))
+        {
+            lives = 3;
+        }
+        else if (CompareTag("Tree"))
+        {
+            lives = 2;
+        }
+        else if (CompareTag("Bush"))
+        {
+            lives = 1;
+        }
+        else
+        {
+            // default fallback
+            lives = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // automatically destroy when lives drop to zero or below
+        if (lives <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
