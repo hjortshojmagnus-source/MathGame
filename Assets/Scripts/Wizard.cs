3cef3deb5b000af2a21f5e75ef738a73f604927f
hjortshojmagnus-source <hjortshojmagnus@gmail.com>
2026-02-27 10:02:37 +0100

    idk

---
+++ Assets/Scripts/Wizard.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Wizard : MonoBehaviour
+{
+    public int health = 75;
+    public int maxHealth = 100;
+    public int damage = 50;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
